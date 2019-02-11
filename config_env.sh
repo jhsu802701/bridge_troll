0b935cda77b3dcdd7b6b90270e4e58ddbfd9d8cd
Jason Hsu <jhsu802701@jasonhsu.com>
2019-02-11 22:43:23 +0000

    Added Google to config_env.sh

--- config_env.sh
+++ config_env.sh
@@ -2,9 +2,9 @@
 
 echo PORT=3000 > .env
 
-echo '*******************************'
-echo 'GitHub App OmniAuth credentials'
-echo '*******************************'
+echo '***************************'
+echo 'GitHub OmniAuth credentials'
+echo '***************************'
 echo '--------------------------------------------------'
 echo 'Follow these steps to get your GitHub credentials:'
 echo '1. Log into your GitHub account.'
@@ -39,9 +39,9 @@ read GITHUB_SECRET
 echo GITHUB_OAUTH_KEY=$GITHUB_ID >> .env
 echo GITHUB_OAUTH_SECRET=$GITHUB_SECRET >> .env
 
-echo '*********************************'
-echo 'Facebook App OmniAuth credentials'
-echo '*********************************'
+echo '*****************************'
+echo 'Facebook OmniAuth credentials'
+echo '*****************************'
 echo '----------------------------------------------------'
 echo 'Follow these steps to get your Facebook credentials:'
 echo '1. Log into your Facebook account.'
@@ -51,6 +51,7 @@ echo '4. If you already created an OAuth app, click on it.'
 echo '   Get your Facebook App ID and App Secret.'
 echo '   Make sure that the port number in your app domain URL'
 echo '   is correct for your Docker container.'
+echo '   If necessary, add the appropriate URL.'
 echo '   Then skip ahead to enter your credentials.'
 echo '5. If you did not already create an OAuth app, click on'
 echo '   My Apps -> Add New App'.
@@ -72,3 +73,41 @@ read FACEBOOK_SECRET
 
 echo FACEBOOK_OAUTH_KEY=$FACEBOOK_ID >> .env
 echo FACEBOOK_OAUTH_SECRET=$FACEBOOK_SECRET >> .env
+
+echo '***************************'
+echo 'Google OmniAuth credentials'
+echo '***************************'
+echo '--------------------------------------------------'
+echo 'Follow these steps to get your Google credentials:'
+echo '1. Log into your Google account.'
+echo '2. Go to https://console.developers.google.com/ .'
+echo '3. Click on "Credentials".'
+echo '4. If you already created an OAuth app, click on it.'
+echo '   Get your Google Client ID and Client Secret.'
+echo '   Make sure that the port number in your app domain URL'
+echo '   is correct for your Docker container.'
+echo '   If necessary, add the appropriate URL.'
+echo '   Then skip ahead to enter your credentials.'
+echo '5. If you did not already create an OAuth app, click on'
+echo '   Create Credentials -> Create OAuth client ID'.
+echo '6. Click on "Web Application".'
+echo '7. Use "bridge_troll" as the name.'
+echo '8. Enter "http://localhost:3000" under Authorized JavaScript origins.'
+echo '   NOTE: If you are using a non-zero offset for the port numbers,'
+echo '   the port number will be different from 3000.'
+echo '9. Enter "http://localhost:3000/users/auth/google_oauth2/callback"'
+echo '   under Authorized redirect URIs.'
+echo '   NOTE: If you are using a non-zero offset for the port numbers,'
+echo '   the port number will be different from 3000.'
+echo '10. Click on "Create".  If all goes well, your'
+echo '    App ID and App Secret are now provided.'
+echo
+echo '++++++++++++++++++++++++++'
+echo 'Enter the Google Client ID:'
+read GOOGLE_ID
+
+echo 'Enter the Google Client Secret:'
+read GOOGLE_SECRET
+
+echo GOOGLE_OAUTH_KEY=$GOOGLE_ID >> .env
+echo GOOGLE_OAUTH_SECRET=$GOOGLE_SECRET >> .env
