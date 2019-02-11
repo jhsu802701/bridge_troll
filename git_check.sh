bb9af636a7f3f640662655cf30e7acf279c2201f
Jason Hsu <jhsu802701@jasonhsu.com>
2019-02-11 16:54:17 +0000

    Removed rubocop and rails_best_practices from git_check.sh

--- git_check.sh
+++ git_check.sh
@@ -10,14 +10,6 @@ echo '---------------------------------------'
 echo 'bundle exec brakeman -Aq -w2 --no-pager'
 bundle exec brakeman -Aq -w2 --no-pager
 
-echo '----------------------'
-echo 'bundle exec rubocop -D'
-bundle exec rubocop -D
-
-echo '----------------------------------'
-echo 'bundle exec rails_best_practices .'
-bundle exec rails_best_practices .
-
 echo '----------'
 echo 'git status'
 git status
